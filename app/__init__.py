6b3f4af6d69b3049c157c2da160f690c70fb04ad
orlasiimwe <orlanerys1@gmail.com>
2020-07-04 05:24:19 +0300

    Code

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Resource, reqparse, Api
+import logging
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
@@ -29,6 +30,9 @@ migrate = Migrate(app, db)
 
 # from .controllers import bp
 # app.register_blueprint(bp)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+
 
 
 
